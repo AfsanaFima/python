fc226ac9a21e8570b57bd827d3aaaae7722dd0aa
AfsanaFima <fifaafsana@gmail.com>
2024-12-19 15:35:29 +0600

    m

---
+++ Tuple_Product.py
@@ -0,0 +1,9 @@
+# Define the tuple
+numbers = (2, 3, 4, 5)
+
+# Calculate the product of all numbers in the tuple
+product = 1
+for num in numbers:
+    product *= num
+
+print(f"The product of all numbers in the tuple: {product}")
